5a0a67eadf3545a5a3c9bd3b179d372a0d3544ee
Marianne van Baren <vanbarenmarianne@gmail.com>
2023-12-14 14:52:02 +0100

    Delete movies 11.sql

--- movies 11.sql
+++
@@ -1,5 +0,0 @@
-SELECT title from movies
-JOIN stars ON movies.id = stars.movie_id
-JOIN people ON stars.person_id = people.id
-JOIN ratings ON movies.id = ratings.movie_id
-WHERE people.name LIKE 'Chadwick Boseman%' ORDER BY rating DESC LIMIT 5;
