a87c0f5fd91942eec0b73fb824e3dc87d8108260
Marianne van Baren <vanbarenmarianne@gmail.com>
2024-03-19 16:04:37 +0100

    Delete finalproject directory

--- finalproject/log.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    email TEXT NOT NULL,
-    hash TEXT NOT NULL
-);
-
-CREATE UNIQUE INDEX username ON users (username);
